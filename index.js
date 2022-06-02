365085e51434732f94b91a93b281ab64f8ddbc1a
Tiago Pujia <tiagonahuelpujia@gmail.com>
2022-06-02 00:43:54 -0300

    Fuente de letras agregada de forma local. Mejos en la estructura HTML. Validación de formulario con Bootstrap agregado. Script completado para el envio de datos, optimización y orden de codigo mejorado.

--- index.js
+++ index.js
@@ -1,50 +1,105 @@
 // Movimiento Barra de progreso
-
 const transformPxNumber = (px) => Number(px.replace("px", ""));
 
-const progressBar = document.querySelector("#progressbar");
-const progress = document.querySelector("#progress");
-let i = 1;
+const activateProgressBar = () => {
+    const progressBar = document.querySelector("#progressbar");
+    const progress = document.querySelector("#progress");
+    let i = 1;
 
-const intervalProgressBar = setInterval(() => {
-    let progressBarWidth = getComputedStyle(progressBar).width;
-    let progressWidth = getComputedStyle(progress).width;
+    setInterval(() => {
+        let progressBarWidth = getComputedStyle(progressBar).width;
+        let progressWidth = getComputedStyle(progress).width;
 
-    if (
-        i >=
-        transformPxNumber(progressBarWidth) - transformPxNumber(progressWidth)
-    ) {
-        i = 0;
-    }
+        if (
+            i >=
+            transformPxNumber(progressBarWidth) -
+                transformPxNumber(progressWidth)
+        ) {
+            i = 0;
+        }
 
-    progress.style.marginLeft = i + "px";
+        progress.style.marginLeft = i + "px";
 
-    i++;
-}, 10);
+        i++;
+    }, 10);
 
-// Previsulizar Imagen del Form
+    return true;
+};
 
+// Previsualizar Imagen en el card3
 const inputFile = document.querySelector("#inputFile"),
-    inputFilePreviewer = document.querySelector("#inputFilePreviewer"),
-    formCard1 = document.querySelector("#formCard1");
+    inputFilePreviewer = document.querySelector("#inputFilePreviewer");
 
-inputFile.addEventListener("change", (e) => {
+const previewImg = () => {
     let reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
+
+    reader.readAsDataURL(inputFile.files[0]);
     reader.addEventListener("load", () => {
         inputFilePreviewer.src = reader.result;
-        formCard1.submit();
     });
-});
+};
 
-// Cambio de Cards y Envio de formulario
+// Envio de Información al servidor
+const formCard1 = document.querySelector("#formCard1"),
+    urlResult = document.querySelector("#urlResult");
 
-// inputFile;
+formCard1.addEventListener("submit", (e) => e.preventDefault());
 
-formCard1.addEventListener("submit", (e) => {
-    e.preventDefault();
+const submitData = () => {
+    let data = new FormData();
+    data.append("img", inputFile.files[0]);
 
-    // fetch("api")
-    //     .then((response) => response.json())
-    //     .then(console.log);
-});
+    let petition = fetch("api/index.php", { method: "POST", body: data });
+
+    return petition;
+};
+
+// Cambio de Card
+const changeCard = (cardID) => {
+    let cards = Array.from(document.querySelectorAll(".card"));
+    let cardShow = document.querySelector(`#${cardID}`);
+
+    cards.forEach((card) => {
+        card.classList.add("d-none");
+    });
+
+    cardShow.classList.remove("d-none");
+
+    return true;
+};
+
+// ===============
+
+const tagShowError = document.querySelector("#formCard1 .invalid-feedback");
+
+const formCompleteSubmit = () => {
+    inputFile.classList.remove("is-invalid");
+    formCard1.classList.remove("was-validated");
+
+    changeCard("cardLoad");
+    activateProgressBar();
+    submitData()
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.status != 1) throw new Error(response.response);
+            return response.response;
+        })
+        .then((response) => {
+            urlResult.value = response;
+            previewImg();
+        })
+        .then(() => 
+            changeCard("cardResult")
+        )
+
+        .catch((error) => {
+            tagShowError.textContent = error;
+
+            inputFile.classList.add("is-invalid");
+            formCard1.classList.add("was-validated");
+
+            changeCard("cardForm");
+        });
+};
+
+inputFile.addEventListener("change", formCompleteSubmit);
